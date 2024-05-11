04101a630009e5e5f0129f1917d41c901452b519
JoaoPec <Joaopecurcino@hotmail.com>
2024-05-11 18:50:15 -0300

    added footer and buttons

---
+++ src/components/MainFooter/MainFooter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./MainFooter.module.css";
+
+const MainFooter = () => {
+    return (
+        <footer className={classes.footer}>
+            <p>LimpaSalvador &copy; 2021</p>
+        </footer>
+    );
+}
+
+export default MainFooter;
--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainHeader from "../../components/MainHeader/MainHeader";
+import MainFooter from "../../components/MainFooter/MainFooter";
 import classes from "./MainPage.module.css";
 
 const MainPage = () => {
@@ -24,7 +25,12 @@ const MainPage = () => {
                         </p>
                     </section>
                 </section>
+                <div className={classes.buttonsContainer}>
+                    <button>Registrar</button>
+                    <button>Login</button>
+                </div>
             </main>
+            <MainFooter />
         </>
     );
 };
