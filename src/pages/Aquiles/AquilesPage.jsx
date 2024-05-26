31ced98a08b741e04b66cc4d3b603e0b1fbb25f1
JoaoPec <joaopecurcino@hotmail.com>
2024-05-25 23:57:40 -0300

    style for peixe-boi

--- src/pages/Aquiles/AquilesPage.jsx
+++ src/pages/Aquiles/AquilesPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
+const imageStyle = {
+    width: "70%",
+    height: "auto",
+    objectFit: "cover",
+    marginBottom: "20px"
+
+}
 
 const AquilesPage = () => {
     return (
         <div>
-            <img src="https://i.natgeofe.com/n/5ebcfa5a-0f66-478a-8bf2-68bc6d09166a/00000168-5d69-d153-af7e-7dfbfa9c0000.jpg?wp=1&w=1280&h=720" alt="" />
+            <img style={imageStyle} src="https://i.natgeofe.com/n/5ebcfa5a-0f66-478a-8bf2-68bc6d09166a/00000168-5d69-d153-af7e-7dfbfa9c0000.jpg?wp=1&w=1280&h=720" alt="" />
             <h1>E ai Aquiles sua PUTA</h1>
         </div>
     );
